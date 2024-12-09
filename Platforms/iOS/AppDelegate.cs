482ae3ca1743f1349ed811312b8a466637704776
SeaJayye <nicolas.ranger@outlook.com>
2024-12-08 21:34:55 -0500

    Initial Commit
    Creating the github repository from a local VS .NET Maui project.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace maui_capstone
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# maui-capstone
+ Functional application using .NET MAUI, focusing on core development principles such as MVVM, API consumption, and SQLite database integration.
